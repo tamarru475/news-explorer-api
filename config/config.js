097ab24ed26b94706be1deff121173ef0ef5e989
tamarru475 <tamarrubin@Tamars-MacBook-Air.local>
2022-11-01 14:31:17 +1300

    corrections after review

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config({ path: '../.env' });
 
 const {
-  NODE_ENV, JWT_SECRET = 'dev-secret', MONGODB_URI = 'mongodb://localhost:27017/newsexplorer', PORT = 3001,
+  NODE_ENV, JWT_SECRET = 'dev-secret', MONGODB_URI = 'mongodb://localhost:27017/newsexplorer', PORT = 3000,
 } = process.env;
 module.exports = {
   NODE_ENV, JWT_SECRET, MONGODB_URI, PORT,
